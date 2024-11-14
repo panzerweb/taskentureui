50b68ee85305b612718adeb516046b5a786f6122
PanzerWeb <selwynvillar@gmail.com>
2024-11-14 15:38:00 +0800

    Optimize the Routing
    Reduce Redundancy of home and starred routes

--- routes/web.php
+++ routes/web.php
@@ -9,35 +9,22 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-//Authentication Route
+// Authentication Routes
 Auth::routes();
 
-//Pages and Home Route for navigational links
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('/pages/starred', [App\Http\Controllers\HomeController::class, 'starred'])->name('pages.starred');
-Route::get('/pages/trash', [App\Http\Controllers\HomeController::class, 'trash'])->name('pages.trash');
-Route::get('/pages/help', [App\Http\Controllers\HomeController::class, 'help'])->name('pages.help');
-
-//Task Routes
+// Task Routes (with Auth Middleware)
 Route::middleware('auth')->group(function () {
-    // Create
-    Route::post('/create-task', [TaskController::class, 'createTask']);
-
-    //=============================
-    // Updates
-    //=============================
-    Route::post('/toggle-complete/{id}', [TaskController::class, 'toggleComplete'])->name('tasks.toggleComplete');
-    Route::put('/edit-task/{task}', [TaskController::class, 'editTask'])->name('tasks.edit');
-
-    //Delete Soft Delete
-    Route::delete('/delete-task/{task}', [TaskController::class, 'deleteTask'])->name('tasks.delete');
-    //Mark as Favorite
-    Route::post('/starred-task/{id}', [TaskController::class, 'markAsFavorite'])->name('tasks.starred');
-
-    //=============================
-    // Display Routes for all pages
-    //=============================
+    // Home and Starred Page Routes
     Route::get('/home', [TaskController::class, 'index'])->name('home');
     Route::get('/pages/starred', [TaskController::class, 'starredIndex'])->name('pages.starred');
-});
+    Route::get('/pages/trash', [App\Http\Controllers\HomeController::class, 'trash'])->name('pages.trash');
+    // Help Route
+    Route::get('/pages/help', [App\Http\Controllers\HomeController::class, 'help'])->name('pages.help');
 
+    // Task CRUD and Actions
+    Route::post('/create-task', [TaskController::class, 'createTask']);
+    Route::post('/toggle-complete/{id}', [TaskController::class, 'toggleComplete'])->name('tasks.toggleComplete');
+    Route::put('/edit-task/{task}', [TaskController::class, 'editTask'])->name('tasks.edit');
+    Route::delete('/delete-task/{task}', [TaskController::class, 'deleteTask'])->name('tasks.delete');
+    Route::post('/starred-task/{id}', [TaskController::class, 'markAsFavorite'])->name('tasks.starred');
+});
